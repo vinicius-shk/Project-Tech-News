2b66416f2e0693f198c753bce74f54680c81259d
NoteVini <vinicius.shk@gmail.com>
2023-03-14 20:20:35 -0300

    feat: Implements the func top_5_categories to list most common categories in DB

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,15 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    response = find_news()
+    cat_counter = dict()
+    for news in response:
+        if news['category'] not in cat_counter:
+            cat_counter[news['category']] = 1
+        cat_counter[news['category']] += 1
+    sorted_counter = sorted(
+        cat_counter.items(), key=lambda x: (-x[1], x[0])
+        )
+    return [key for key, value in sorted_counter]
