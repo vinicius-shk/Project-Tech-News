ea46d4813f4f364caacd7dea05556f6ab98c825e
NoteVini <vinicius.shk@gmail.com>
2023-03-14 18:49:30 -0300

    feat: Implements a func to search on DB based on ISO date format

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,15 +1,26 @@
 from tech_news.database import search_news
+from datetime import datetime
+
+
+def factory_search(news: list):
+    return [(item['title'], item['url']) for item in news]
 
 
 # Requisito 7
 def search_by_title(title: str):
     response = search_news({'title': {"$regex": f'{title}', "$options": "i"}})
-    return [(item['title'], item['url']) for item in response]
+    return factory_search(response)
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, '%Y-%m-%d')
+        news_date = f'{date[8:]}/{date[5:7]}/{date[:4]}'
+        response = search_news({'timestamp': f'{news_date}'})
+        return factory_search(response)
+    except ValueError:
+        raise ValueError('Data inválida')
 
 
 # Requisito 9
